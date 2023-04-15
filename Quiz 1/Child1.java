72db69ba82ec1232f965df93a054c839f5681689
markcaram47 <111546363+markcaram47@users.noreply.github.com>
2023-04-15 17:41:39 +0800

    Create Child1.java

---
+++ Quiz 1/Child1.java
@@ -0,0 +1,12 @@
+
+public class Child1 extends Parent {
+    String Name, Birthday;
+    int Age;
+    
+    void childAttributes (){
+        System.out.println("Name: " + Name);
+        System.out.println("Age: " + Age);
+        System.out.println("Birthday: " + Birthday);
+        System.out.println("");
+    }
+}
